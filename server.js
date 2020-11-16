a0b198931ef28417a51871ef3004b4ffdd49f3d8
Itay <naveh.itay@gmail.com>
2020-11-16 17:02:03 +0000

    making the site secure-er

--- server.js
+++ server.js
@@ -39,9 +39,9 @@ const app = express();
 
 app.use(express.json());
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET", "POST");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Methods", "GET", "POST");
+    // res.header("Access-Control-Allow-Headers", "Content-Type");
     // res.header("Access-Control-Allow-Headers", "*");
     next();
 });
